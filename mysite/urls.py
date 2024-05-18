c261ce74c831b711bccf5689c9cde7a501f11820
Yarik <57310769+DeFosu@users.noreply.github.com>
2024-05-18 18:17:19 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,5 @@ from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     path(_('admin/'), admin.site.urls),
-    path('rosetta/', include('rosetta.urls')),
     path('', include('journal.urls')),
 )
